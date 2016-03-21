952530da06f06489877a60ad25f406318d78daad
Alex Holt <berber.it@gmail.com>
2016-03-21 12:59:52 +0200

    Test query has been added

--- app.py
+++ app.py
@@ -7,5 +7,22 @@ from common.MysqlConnector import MysqlConnector
 from scripts.RadioSpider import RadioSpider
 from utils.Utils import Utils
 
+class App(object):
+
+    def app_run(self):
+
+        """test query"""
+
+        testQuery = 'SELECT * from stations;'
+        mysqlRun = MysqlConnector()
+        mysqlRun.get_select(testQuery)
+
 if "__name__" == '__main__':
-    pass
+
+    appRun = App()
+    appRun.app_run()
+
+
+
+
+
