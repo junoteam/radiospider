c5695f09fc6013c4f124b01cfb702bb968c21a5d
Alex Holt <berber.it@gmail.com>
2016-03-22 17:52:57 +0200

    Array with genres is ready Some imports removed Code cleaning

--- scripts/RadioSpider.py
+++ scripts/RadioSpider.py
@@ -2,23 +2,21 @@
 # -*- coding: utf-8 -*-
 # -*- by Alex -*-
 
-import lxml.html
-from lxml import etree
-from io import StringIO, BytesIO
-from common.MysqlConnect import MysqlConnect
 from urllib2 import urlopen
 from BeautifulSoup import BeautifulSoup
+import re
 
 class RadioSpider(object):
 
     def RadioParser(self):
+
+        genres_array = []
 
         radio_url_genre = 'http://vtuner.com/setupapp/guide/asp/BrowseStations/BrowsePremiumStations.asp?sCategory=Alternative&sBrowseType=Format&sViewBy=&sSortby=&sWhatList=&sNiceLang=&iCurrPage=1'
         radio_url_format = 'http://vtuner.com/setupapp/guide/asp/BrowseStations/StartPage.asp?sBrowseType=Format'
         soup = BeautifulSoup(urlopen(radio_url_format).read())
 
         #raw_html = soup.prettify()[0:1000000]
-        #print raw_html
 
         genre_rows = soup.find('table', id='table10').findAll('tr')
         for row in genre_rows:
@@ -27,6 +25,8 @@ class RadioSpider(object):
                 t = row_2.findAll('a')
                 for row_3 in t:
                     z = row_3.getText()
-                    print z
+                    matchObj = re.sub('\s\([\d]*\)\s?', '', z)
+                    genres_array.append(matchObj)
 
+        print genres_array
 
