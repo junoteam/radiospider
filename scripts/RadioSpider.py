c898a844e4ea16b17ac2309734e4967d8e3c4464
Alex Holt <berber.it@gmail.com>
2016-03-22 18:10:56 +0200

    RadioSpider class refactor to few functions

--- scripts/RadioSpider.py
+++ scripts/RadioSpider.py
@@ -8,14 +8,13 @@ import re
 
 class RadioSpider(object):
 
-    def RadioParser(self):
+    radio_url_genre = 'http://vtuner.com/setupapp/guide/asp/BrowseStations/BrowsePremiumStations.asp?sCategory=Alternative&sBrowseType=Format&sViewBy=&sSortby=&sWhatList=&sNiceLang=&iCurrPage=1'
+    radio_url_format = 'http://vtuner.com/setupapp/guide/asp/BrowseStations/StartPage.asp?sBrowseType=Format'
+
+    def genresParser(self):
 
         genres_array = []
-
-        radio_url_genre = 'http://vtuner.com/setupapp/guide/asp/BrowseStations/BrowsePremiumStations.asp?sCategory=Alternative&sBrowseType=Format&sViewBy=&sSortby=&sWhatList=&sNiceLang=&iCurrPage=1'
-        radio_url_format = 'http://vtuner.com/setupapp/guide/asp/BrowseStations/StartPage.asp?sBrowseType=Format'
-        soup = BeautifulSoup(urlopen(radio_url_format).read())
-
+        soup = BeautifulSoup(urlopen(self.radio_url_format).read())
         #raw_html = soup.prettify()[0:1000000]
 
         genre_rows = soup.find('table', id='table10').findAll('tr')
@@ -28,5 +27,8 @@ class RadioSpider(object):
                     matchObj = re.sub('\s\([\d]*\)\s?', '', z)
                     genres_array.append(matchObj)
 
-        print genres_array
+        return genres_array
+
+    def radioParser(self, genre):
+        pass
 
