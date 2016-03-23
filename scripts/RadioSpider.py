0a5d24b644932fa46522dd23291de76cd162daa2
Alex Holt <berber.it@gmail.com>
2016-03-23 13:15:32 +0200

    All special characters removed by regex and replace

--- scripts/RadioSpider.py
+++ scripts/RadioSpider.py
@@ -25,8 +25,9 @@ class RadioSpider(object):
                 for row_3 in t:
                     z = row_3.getText()
                     matchObj = re.sub('[;]?\s\([\d]*\)\s?', '', z)
+                    if matchObj == 'R&B':
+                        matchObj = matchObj.replace('&', '%26')
                     genres_array.append(matchObj)
-
         return genres_array
 
     def radioParser(self):
@@ -35,6 +36,10 @@ class RadioSpider(object):
         for item in range(len(all_genres)):
             radio_url_genre = 'http://vtuner.com/setupapp/guide/asp/BrowseStations/BrowsePremiumStations.asp?sCategory=' + all_genres[item] + '&sBrowseType=Format&sViewBy=&sSortby=&sWhatList=&sNiceLang=&iCurrPage=1'
             print radio_url_genre
+
+
+
+
 
         uuuur = 'http://vtuner.com/setupapp/guide/asp/BrowseStations/BrowsePremiumStations.asp?sCategory=R&B&sBrowseType=Format&sViewBy=&sSortby=&sWhatList=&sNiceLang=&iCurrPage=1'
         #filehandle = urllib.urlopen(uuuur).read()
