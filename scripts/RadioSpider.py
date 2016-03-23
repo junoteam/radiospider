2d2bcdeee9ea63ebe10b7da77fa05b8b3a37d912
Alex Holt <berber.it@gmail.com>
2016-03-23 12:33:34 +0200

    Get all genre URLs

--- scripts/RadioSpider.py
+++ scripts/RadioSpider.py
@@ -5,10 +5,10 @@
 from urllib2 import urlopen
 from BeautifulSoup import BeautifulSoup
 import re
+import urllib
 
 class RadioSpider(object):
 
-    radio_url_genre = 'http://vtuner.com/setupapp/guide/asp/BrowseStations/BrowsePremiumStations.asp?sCategory=Alternative&sBrowseType=Format&sViewBy=&sSortby=&sWhatList=&sNiceLang=&iCurrPage=1'
     radio_url_format = 'http://vtuner.com/setupapp/guide/asp/BrowseStations/StartPage.asp?sBrowseType=Format'
 
     def genresParser(self):
@@ -24,13 +24,33 @@ class RadioSpider(object):
                 t = row_2.findAll('a')
                 for row_3 in t:
                     z = row_3.getText()
-                    matchObj = re.sub('\s\([\d]*\)\s?', '', z)
+                    matchObj = re.sub('[;]?\s\([\d]*\)\s?', '', z)
                     genres_array.append(matchObj)
 
         return genres_array
 
     def radioParser(self):
 
-        a = self.genresParser()
-        print a
+        all_genres = self.genresParser()
+        for item in range(len(all_genres)):
+            radio_url_genre = 'http://vtuner.com/setupapp/guide/asp/BrowseStations/BrowsePremiumStations.asp?sCategory=' + all_genres[item] + '&sBrowseType=Format&sViewBy=&sSortby=&sWhatList=&sNiceLang=&iCurrPage=1'
+            print radio_url_genre
+
+        uuuur = 'http://vtuner.com/setupapp/guide/asp/BrowseStations/BrowsePremiumStations.asp?sCategory=R&B&sBrowseType=Format&sViewBy=&sSortby=&sWhatList=&sNiceLang=&iCurrPage=1'
+        #filehandle = urllib.urlopen(uuuur).read()
+        #print filehandle
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
