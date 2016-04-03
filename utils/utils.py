3081e2303b3ad6cbf83faa8a69ae80aa0d82dffb
alex <berber.it@gmail.com>
2016-04-04 00:17:59 +0300

    List of states for USA & Canada added as JSON example

--- utils/utils.py
+++ utils/utils.py
@@ -30,3 +30,106 @@ class Utils():
                 text[:-1] = ""
         return text
 
+    # Only lists of states for USA & Canada
+    @staticmethod
+    def states_usa_list():
+        states = {
+            'AK': 'Alaska',
+            'AL': 'Alabama',
+            'AR': 'Arkansas',
+            'AS': 'American Samoa',
+            'AZ': 'Arizona',
+            'CA': 'California',
+            'CO': 'Colorado',
+            'CT': 'Connecticut',
+            'DC': 'District of Columbia',
+            'DE': 'Delaware',
+            'FL': 'Florida',
+            'GA': 'Georgia',
+            'GU': 'Guam',
+            'HI': 'Hawaii',
+            'IA': 'Iowa',
+            'ID': 'Idaho',
+            'IL': 'Illinois',
+            'IN': 'Indiana',
+            'KS': 'Kansas',
+            'KY': 'Kentucky',
+            'LA': 'Louisiana',
+            'MA': 'Massachusetts',
+            'MD': 'Maryland',
+            'ME': 'Maine',
+            'MI': 'Michigan',
+            'MN': 'Minnesota',
+            'MO': 'Missouri',
+            'MP': 'Northern Mariana Islands',
+            'MS': 'Mississippi',
+            'MT': 'Montana',
+            'NA': 'National',
+            'NC': 'North Carolina',
+            'ND': 'North Dakota',
+            'NE': 'Nebraska',
+            'NH': 'New Hampshire',
+            'NJ': 'New Jersey',
+            'NM': 'New Mexico',
+            'NV': 'Nevada',
+            'NY': 'New York',
+            'OH': 'Ohio',
+            'OK': 'Oklahoma',
+            'OR': 'Oregon',
+            'PA': 'Pennsylvania',
+            'PR': 'Puerto Rico',
+            'RI': 'Rhode Island',
+            'SC': 'South Carolina',
+            'SD': 'South Dakota',
+            'TN': 'Tennessee',
+            'TX': 'Texas',
+            'UT': 'Utah',
+            'VA': 'Virginia',
+            'VI': 'Virgin Islands',
+            'VT': 'Vermont',
+            'WA': 'Washington',
+            'WI': 'Wisconsin',
+            'WV': 'West Virginia',
+            'WY': 'Wyoming'
+        }
+        pass
+
+    @staticmethod
+    def states_canada_list():
+        prov_terr = {
+            'AB': 'Alberta',
+            'BC': 'British Columbia',
+            'MB': 'Manitoba',
+            'NB': 'New Brunswick',
+            'NL': 'Newfoundland and Labrador',
+            'NT': 'Northwest Territories',
+            'NS': 'Nova Scotia',
+            'NU': 'Nunavut',
+            'ON': 'Ontario',
+            'PE': 'Prince Edward Island',
+            'QC': 'Quebec',
+            'SK': 'Saskatchewan',
+            'YT': 'Yukon'
+        }
+
+        provinces = {
+            'AB': 'Alberta',
+            'BC': 'British Columbia',
+            'MB': 'Manitoba',
+            'NB': 'New Brunswick',
+            'NL': 'Newfoundland and Labrador',
+            'NS': 'Nova Scotia',
+            'ON': 'Ontario',
+            'PE': 'Prince Edward Island',
+            'QC': 'Quebec',
+            'SK': 'Saskatchewan'
+        }
+
+        territories = {
+            'NT': 'Northwest Territories',
+            'NU': 'Nunavut',
+            'YT': 'Yukon'
+        }
+        pass
+
+
